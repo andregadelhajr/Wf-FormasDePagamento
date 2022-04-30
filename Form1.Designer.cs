6ee778edffca4673029651470ffbb27eb8dc9932
Andre junior <andrrejunior@hotmail.com.br>
2022-04-29 22:54:11 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Aula40
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEscolha = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formas de Pagamento";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 116);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Método de Pagamento:";
+            // 
+            // lblEscolha
+            // 
+            this.lblEscolha.AutoSize = true;
+            this.lblEscolha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscolha.Location = new System.Drawing.Point(194, 167);
+            this.lblEscolha.Name = "lblEscolha";
+            this.lblEscolha.Size = new System.Drawing.Size(44, 16);
+            this.lblEscolha.TabIndex = 3;
+            this.lblEscolha.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valor";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(295, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 160);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 238);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEscolha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEscolha;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula40
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //criar uma arrayList
+            ArrayList formaPagto = new ArrayList();
+            formaPagto.Add(new FormaDePagto(1, "Selecionar Opção"));
+            formaPagto.Add(new FormaDePagto(2, "Cartão"));
+            formaPagto.Add(new FormaDePagto(3, "Boleto"));
+            formaPagto.Add(new FormaDePagto(4, "Pix"));
+            formaPagto.Add(new FormaDePagto(5, "Dinheiro"));
+
+            //vincular ao comboBox
+            comboBox1.DataSource = formaPagto;
+            comboBox1.DisplayMember = "Descricao";
+            comboBox1.ValueMember = "ID";
+
+            int op = comboBox1.SelectedIndex;
+            lblEscolha.Text = op.ToString();
+
+        }
+
+        public class FormaDePagto
+        {
+            public int ID { get; set; }
+            public string Descricao { get; set; }
+
+            public FormaDePagto(int ID, string Descricao)
+            {
+                this.ID = ID;
+                this.Descricao = Descricao;
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int op = comboBox1.SelectedIndex;
+            lblEscolha.Text = op.ToString();
+
+            switch (op)
+            {
+                case 0:
+                    lblEscolha.Text = "Selecionar Opção";
+                    break;
+                case 1:
+                    lblEscolha.Text = "Cartão";
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add($"1x {double.Parse(textBox1.Text):F2}");
+                    listBox1.Items.Add($"2x {((double.Parse(textBox1.Text)) / 2):F2}");
+                    listBox1.Items.Add($"3x {((double.Parse(textBox1.Text)) / 3):F2}");
+                    listBox1.Items.Add($"4x {((double.Parse(textBox1.Text)) / 4):F2}");
+                    listBox1.Items.Add($"5x {((double.Parse(textBox1.Text)) / 5):F2}");
+                    listBox1.Items.Add($"6x {((double.Parse(textBox1.Text)) / 6):F2}");
+                    listBox1.Items.Add($"7x {((double.Parse(textBox1.Text)) / 7):F2}");
+                    listBox1.Items.Add($"8x {((double.Parse(textBox1.Text)) / 8):F2}");
+                    listBox1.Items.Add($"9x {((double.Parse(textBox1.Text)) / 9):F2}");
+                    listBox1.Items.Add($"10x {((double.Parse(textBox1.Text)) / 10):F2}");
+                    listBox1.Items.Add($"11x {((double.Parse(textBox1.Text)) / 11):F2}");
+                    listBox1.Items.Add($"12x {((double.Parse(textBox1.Text)) / 12):F2}");
+                    break;
+                case 2:
+                    lblEscolha.Text = "Boleto";
+                    break;
+                case 3:
+                    lblEscolha.Text = "Pix";
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add("Chave Pix");
+                    break;
+                case 4:
+                    lblEscolha.Text = "Dinheiro";
+                    break;
+                default:
+                    lblEscolha.Text = "";
+                    break;
+            }
+        }
+    }
+}
